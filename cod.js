77e99aa9251a8f9fc72dc4932ddd3b00ea5aaddb
MATHSYSTM <144828436+MATHSYSTM@users.noreply.github.com>
2024-05-11 13:09:46 +0000

    Add files via upload

---
+++ cod.js
@@ -0,0 +1,5 @@
+gatekey="123459876";
+
+document.querySelector("body").removeChild(document.querySelector("#change"));
+
+document.querySelector("body").removeChild(document.querySelector("#newcod"));
